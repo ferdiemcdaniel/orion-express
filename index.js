58e4c4b1e542098a7f4e69eb71033a3c078b3aad
Ferdinand McDaniel <imcdanielamano@gmail.com>
2017-09-24 16:17:23 +0800

    [4/11 express egghead] updated server with links, new views

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (request, result) => {
-    result.send('Hello World!');
+var fs = require('fs');
+var _ = require('lodash');
+var engines = require('consolidate');
+var users = [];
+
+fs.readFile('users.json', {encoding: 'utf8'}, (err, data) => {
+    if (err) throw err;
+
+    JSON.parse(data).forEach(user => {
+        user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
+        users.push(user);
+    });
+});
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+app.engine('hbs', engines.handlebars);
+
+app.get('/', (req, res) => {
+    res.render('index', {users: users});
+});
+
+app.get(/big.*/, (req, res, next) => {
+    next();
+});
+
+app.get('/:username', (req, res) => {
+    let username = req.params.username;
+    res.send(username);
 });
 
 app.get('/yo', (request, result) => {
